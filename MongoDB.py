9c609ec43d4d04d52447e13a5a63e1f0a56eb97b
Alexsashka43 <63096544+Alexsashka43@users.noreply.github.com>
2021-01-14 18:08:06 +0300

    Add files via upload

---
+++ MongoDB.py
@@ -0,0 +1,104 @@
+import csv
+import re
+from pprint import pprint
+from pymongo import MongoClient
+import pymongo
+from _datetime import datetime
+
+
+
+def date_sort(db, first_date, second_date):
+    fd = from_string_to_datetime(first_date)
+    sd = from_string_to_datetime(second_date)
+    if fd > sd:
+        print('Попробуйте ещё раз')
+    else:
+        sorted_database = db.collection.find({'Дата':{'$lte': sd, '$gte': fd}}).sort('Дата', 1)
+        for i in sorted_database:
+            print(i)
+
+
+def from_string_to_datetime(string_with_date):
+    date_massive = ['2020']
+    date_massive += string_with_date.split('.')
+    true_massive = []
+    for i in date_massive:
+        true_massive.append(int(i))
+    datetime_obj = datetime(true_massive[0], true_massive[2], true_massive[1])
+    return datetime_obj
+
+
+def read_data(csv_file, db):
+    """
+    Загрузить данные в бд из CSV-файла
+    """
+    artists_list = []
+    with open(csv_file, encoding='utf8') as csvfile:
+
+        reader = csv.DictReader(csvfile)
+
+        for row in reader:
+            artists_list.append(row)
+
+        for person in artists_list:
+            price = person['Цена']
+            person['Цена'] = int(price)
+            person['Дата'] = from_string_to_datetime(person['Дата'])
+
+    #collection - коллекция
+
+    db.collection.insert_many(artists_list)
+
+
+def delete_data(db):
+    """
+    Удалиль коллекцию
+    Документация: https://docs.mongodb.com/manual/reference/method/db.collection.drop/index.html
+    """
+
+    db.collection.drop()
+
+
+def find_cheapest(db):
+    """
+    Отсортировать билеты из базы по возрастанию цены
+    Документация: https://docs.mongodb.com/manual/reference/method/cursor.sort/
+    """
+
+    sorted_database = db.collection.find().sort('Цена', 1)
+    for i in sorted_database:
+        #print(i)  # Для сортировки по цене уберите (#) перед print
+        pass
+
+
+def find_by_name(name, db):
+    """
+    Найти билеты по имени исполнителя (в том числе – по подстроке, например "Seconds to"),
+    и вернуть их по возрастанию цены
+    """
+
+    regex = re.compile(f'(.)*({name})(.)*')
+    sorted_database = db.collection.find({'Исполнитель': regex}).sort('Цена', 1)
+
+    for i in sorted_database:
+        print(i)
+
+if __name__ == '__main__':
+
+    client = pymongo.MongoClient()
+
+    music_artists_db = client['m_artists']#база данных
+
+    delete_data(music_artists_db)#удалить коллекцию из БД
+
+    read_data('artists.csv', music_artists_db) #передал данные из csv в БД
+
+    find_cheapest(music_artists_db)#
+
+    find_by_name(input('Введите название группы или имя исполнителя:'), music_artists_db)
+
+    first_date = input('Введите с какого числа нужно вести поиск(без года, через точку , сначала число , потом месяц):')
+
+    second_date = input('Введите по какое число нужно вести поиск(без года, через точку , сначала число , потом месяц):')
+
+    date_sort(music_artists_db, first_date, second_date)
